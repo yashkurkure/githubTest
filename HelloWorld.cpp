07c5b4e9f27489dea4394acbb5fb78b300689ab5
yashkurkure <yash.kurkure1903@gmail.com>
2021-05-11 19:29:25 +0530

    Hello World in C++

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+//Test Program for Git
+//Starting with a Hello World Program brings good luck :) - Yash May11,2021
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout<<"Hello World"<<"\n";
+	std::cout<<"I was created on May 11, 2021"<<"\n";
+	std::cout<<"\t~By Yash Kurkure"<<"\n";
+	return 0;
+}
